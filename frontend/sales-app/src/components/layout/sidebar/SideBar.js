73bb64f40b7d527bca5963e3bae1043e6af127e1
delalluyah <princefold@gmail.com>
2020-05-23 12:33:19 +0000

    Dasboard update

--- frontend/sales-app/src/components/layout/sidebar/SideBar.js
+++ frontend/sales-app/src/components/layout/sidebar/SideBar.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import './sidebar.css'
-import user from './user.svg'
+import React from "react";
+import "./sidebar.css";
+import user from "./user.svg";
 
-export default function SideBar({ fullname = '', role = '' }) {
+export default function SideBar({ fullname = "", role = "" }) {
   return (
     <div className="sidebar">
       <div className="user">
@@ -25,52 +25,18 @@ export default function SideBar({ fullname = '', role = '' }) {
           <i className="fas fa-chart-line"></i> <a href="/">Dashboard</a>
         </li>
         <li className="side-menu-item">
+          <i className="fas fa-cart-arrow-down"></i>
+          <a href="/record-sales">Record Sales</a>
+        </li>
+        <li className="side-menu-item">
           <i className="fas fa-shopping-cart"></i>
           <a href="/products">View Inventory</a>
         </li>
         <li className="side-menu-item">
           <i className="fas fa-cart-plus"></i>
-          <a href="/add-products">Add Stock</a>
-        </li>
-        <li className="side-menu-item">
-          <i className="fas fa-cart-arrow-down"></i>
-          <a href="/record-sales">Record Sales</a>
-        </li>
-        <li className="side-menu-parent">
-          <ul>
-            <p>
-              {' '}
-              <i className="fas fa-user-cog"></i> Users{' '}
-              <i className="right fas fa-angle-down"></i>
-            </p>
-            <li className="side-menu-item side-menu-child">
-              <i className="fas fa-users"></i>
-              <a href="/users">All Users</a>
-            </li>
-            <li className="side-menu-item side-menu-child">
-              <i className="fas fa-user-plus"></i>
-              <a href="/add-user">Add New User</a>
-            </li>
-          </ul>
-        </li>
-        <li className="side-menu-parent">
-          <ul>
-            <p>
-              {' '}
-              <i className="fas fa-cog"></i>Configurations{' '}
-              <i className="right fas fa-angle-down"></i>
-            </p>
-            <li className="side-menu-item side-menu-child">
-              <i className="fas fa-list-ul"></i>
-              <a href="/categories">Product Categories</a>
-            </li>
-            <li className="side-menu-item side-menu-child">
-              <i className="fas fa-sliders-h"></i>
-              <a href="/types">Product Types</a>
-            </li>
-          </ul>
+          <a href="/add-products">Sales History</a>
         </li>
       </ul>
     </div>
-  )
+  );
 }
